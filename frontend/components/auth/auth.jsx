125a477fbbde7b42d5bd476fab7381a766232442
seanchowdhury <seanchowdhury@gmail.com>
2017-06-28 14:45:17 -0400

    Added demo user

--- frontend/components/auth/auth.jsx
+++ frontend/components/auth/auth.jsx
@@ -16,6 +16,7 @@ class Auth extends React.Component {
       lname: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.demoLogIn = this.demoLogIn.bind(this);
   }
 
   isLogin(){
@@ -49,11 +50,21 @@ class Auth extends React.Component {
     this.props.processForm({user});
   }
 
+  demoLogIn() {
+    const user = {
+      email: 'Scruffy',
+      password: 'password'
+    }
+
+    this.props.processForm({user});
+  }
+
   renderFields() {
     if (this.isLogin()) {
       return (
         <div className='form-items'>
           <h2 className='login-title'>Log in</h2>
+          <button className='auth-button' onClick={this.demoLogIn}>Demo log in</button>
           <p className='enticing-text'>Log in with email</p>
           <input type="text"
             value={this.state.email}
